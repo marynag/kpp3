a837c120f71a204a3ff3bfbc095e96e16c425757
marynag <mgrabchak1109@gmail.com>
2022-05-26 18:55:10 +0300

    Add PostService

--- PostController.js
+++ PostController.js
@@ -1,11 +1,10 @@
-import Post from "./Post.js"
-
+import Post from "./Post.js";
+import PostService from "./PostService.js";
 
 class PostController{
     async create(req,res){
         try{
-            const {author, title, content,picture}= req.body
-            const post = await Post.create( {author, title, content,picture})
+            const post = await PostService.create(req.body)
             res.json(post)
         }catch(e){
             res.status(500).json(e)
@@ -14,7 +13,7 @@ class PostController{
 
     async getAll(req, res){
         try{
-            const posts=await Post.find();
+            const posts=await PostService.getAll()
             return res.json(posts)
         }catch(e){
             res.status(500).json(e)
@@ -22,21 +21,25 @@ class PostController{
     }
     async getOne(req, res){
         try{
-
+            const post=await PostService.getOne(req.params.id)
+            return res.json(post)
         }catch(e){
             res.status(500).json(e)
         }
     }
     async update(req, res){
         try{
-
+           
+            const updatedPost= await PostService.update(req.body)
+            return res.json(updatedPost)
         }catch(e){
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
     }
     async delete(req, res){
-        try{
-
+        try{            
+            const post = await PostService.delete(req.params.id)
+            return res.json(post)
         }catch(e){
             res.status(500).json(e)
         }
